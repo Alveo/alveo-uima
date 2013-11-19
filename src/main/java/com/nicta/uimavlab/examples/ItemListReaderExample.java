937ad17e530afd47664faebb38a145fd93ebd436
Andy MacKinlay <andrew.mackinlay@nicta.com.au>
2013-11-20 10:16:35 +1100

    write descriptors out by default to allow for using e.g. a CPE GUI pipeline (as the auto-gend descriptors currently don’t import the type system)

--- src/main/java/com/nicta/uimavlab/examples/ItemListReaderExample.java
+++ src/main/java/com/nicta/uimavlab/examples/ItemListReaderExample.java
@@ -1,5 +1,7 @@
 package com.nicta.uimavlab.examples;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.apache.uima.UIMAException;
@@ -8,16 +10,18 @@ import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.xml.sax.SAXException;
 
 import com.nicta.uimavlab.ItemListCollectionReader;
 
 public class ItemListReaderExample {
 
-	public static void main(String[] args) throws IOException, UIMAException {
+	public static void main(String[] args) throws IOException, UIMAException, SAXException {
 		String serverUri = args[0];
 		String apiKey = args[1];
 		String itemListId = args[2];
 		String outputDir = args[3];
+		String descriptorDir = args[4];
 
 		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(
 				ItemListCollectionReader.class, 
@@ -25,9 +29,11 @@ public class ItemListReaderExample {
 				ItemListCollectionReader.PARAM_VLAB_API_KEY, apiKey,
 				ItemListCollectionReader.PARAM_VLAB_ITEM_LIST_ID, itemListId,
 				ItemListCollectionReader.PARAM_INCLUDE_RAW_DOCS, false);
+		reader.toXML(new FileOutputStream(new File(descriptorDir, "ItemListCollectionReader.xml")));
 		AnalysisEngineDescription casWriter = AnalysisEngineFactory.createEngineDescription(
 				XmiWriterCasConsumer.class,
 				XmiWriterCasConsumer.PARAM_OUTPUTDIR, outputDir);
+		casWriter.toXML(new FileOutputStream(new File(descriptorDir, "ItemListToXmiAE.xml")));
 		SimplePipeline.runPipeline(reader, casWriter);
 	}
 }
