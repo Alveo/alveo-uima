2c0e84bbe862945bf2dd8dcc8beac6b6a4b0552d
Andrew MacKinlay <am@akmy.net>
2014-04-14 17:42:02 +1000

    changed signature for UIMA-Alveo converter interface - adding another exception for when the annotation type is invalid, and removing an unneeded exception

--- src/main/java/com/nicta/uimavlab/conversions/UIMAToAlveoAnnConverter.java
+++ src/main/java/com/nicta/uimavlab/conversions/UIMAToAlveoAnnConverter.java
@@ -9,9 +9,9 @@ import org.apache.uima.cas.text.AnnotationFS;
  * Created by amack on 11/04/14.
  */
 public interface UIMAToAlveoAnnConverter {
-	void setTypeSystem(TypeSystem ts) throws AnalysisEngineProcessException;
+	void setTypeSystem(TypeSystem ts);
 
-	public TextRestAnnotation convertToAlveo(AnnotationFS ann) throws NotInitializedException;
+	public TextRestAnnotation convertToAlveo(AnnotationFS ann) throws NotInitializedException, InvalidAnnotationTypeException;
 
 	public String getAlveoTypeUriForTypeName(String uimaTypeName);
 
@@ -22,6 +22,13 @@ public interface UIMAToAlveoAnnConverter {
 	 */
 	public class NotInitializedException extends Exception {
 		public NotInitializedException(String s) {
+			super(s);
+		}
+	}
+
+	public class InvalidAnnotationTypeException extends Exception {
+		public InvalidAnnotationTypeException(String s) {
+			super(s);
 		}
 	}
 }
